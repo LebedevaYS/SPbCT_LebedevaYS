1cbd0c6223f675686a5af366041463304fd2f8a8
LebedevaYS <51288465+LebedevaYS@users.noreply.github.com>
2021-10-21 11:57:57 +0300

    Add files via upload

---
+++ Lab23_Ex5/Lab23_Ex5/Lab23_Ex5.cpp
@@ -0,0 +1,20 @@
+﻿// example 5.1
+//#include <stdlib.h>
+//#include <stdio.h>
+//int main() {
+//	char* some_memory = (char*)0;
+//	printf("A read from null %sn", some_memory);
+//	sprintf(some_memory, "A write to nulln");
+//	exit(EXIT_SUCCESS);
+//}	
+
+// example 5.2
+
+#include <stdlib.h>
+#include <stdio.h>
+int main()
+{
+	char z = *(const char*)0;
+	printf("I read from location zeron \n");
+	exit(EXIT_SUCCESS);
+}
